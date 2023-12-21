052281ee5bffbbcd5b2497e3d5d9329edcac33ac
viet-nv <vietnv1304@gmail.com>
2023-12-21 15:14:29 +0700

    init

---
+++ public/libs/GeoGebra/HTML5/5.0/web/deferredjs/DC0558508FD2BD5F33E98C14DC2A0A9A/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.web.runAsyncCallback2('Jhc(3450,1,Bym);_.zd=function(){};tNl(Tc)(2);\n//# sourceURL=web-2.js\n')
---
+++ public/libs/GeoGebra/HTML5/5.0/webSimple/deferredjs/CF03DB06D8ED6C26A2946DC96C813271/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.webSimple.runAsyncCallback2('zqb(2081,1,fGh);_.ld=function(){};r5g(Tc)(2);\n//# sourceURL=webSimple-2.js\n')
